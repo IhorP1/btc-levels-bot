9a10d71d7029b6cc89a60cd55c1cd723f244718d
IhorP1 <ihorpinchuk@hotmail.com>
2025-12-14 22:05:32 +0200

    Remove sensitive Telegram bot configuration
    Removed Telegram bot token and channel ID from config.

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-# Telegram Bot Token (получен от @BotFather)
-TELEGRAM_BOT_TOKEN = "8420252189:AAH4LLMGp3kZIVxmm3R9T8k-7o5lknc5ZCg"
 
-# ID твоего канала или группы (бот должен быть администратором с правом публиковать сообщения)
-TELEGRAM_CHANNEL_ID = "-1003318016772"
