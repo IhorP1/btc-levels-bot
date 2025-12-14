15e2678f98caadfaa94dada5840649fa7f9c29bb
IhorP1 <ihorpinchuk@hotmail.com>
2025-12-14 22:02:56 +0200

    Add Telegram bot token and channel ID to config

---
+++ config.py
@@ -0,0 +1,5 @@
+# Telegram Bot Token (получен от @BotFather)
+TELEGRAM_BOT_TOKEN = "8420252189:AAH4LLMGp3kZIVxmm3R9T8k-7o5lknc5ZCg"
+
+# ID твоего канала или группы (бот должен быть администратором с правом публиковать сообщения)
+TELEGRAM_CHANNEL_ID = "-1003318016772"
